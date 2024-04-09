334cccf278450e275c79685588f69780b0a379a0
ChriCore99 <christian.corelli.io@gmail.com>
2024-04-09 15:12:37 +0200

    ADD: fixed pagine create in .vue

---
+++ vite-start/src/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "La mia prima volta con VITE"
+        }
+    }
+}
+</script>
+
+<template>
+<div id="AppHero">
+    <img src="../public/1618197067vitejs.webp" alt="">
+</div>
+</template>
+
+<style>
+.center{
+    text-align: center;
+}
+</style>
---
+++ vite-start/src/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "La mia prima volta con VITE"
+        }
+    }
+}
+</script>
+
+<template>
+<div id="AppTitle">
+    <h1 class="center">
+        {{ message }}
+    </h1>
+</div>
+</template>
+
+<style>
+.center{
+    text-align: center;
+}
+</style>
