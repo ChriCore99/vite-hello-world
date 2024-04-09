56f5cee80166bdef443342ca2b730836fdfa72b2
ChriCore99 <christian.corelli.io@gmail.com>
2024-04-09 14:59:01 +0200

    ADD: mi serve un terapista...

---
+++ vite-start/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "La mia prima volta con VITE"
+        }
+    }
+}
+</script>
+
+<template>
+<div id="AppTitle">
+    <h1 class="center">
+        {{ message }}
+    </h1>
+</div>
+
+<div id="AppHero">
+    <img src="../public/1618197067vitejs.webp" alt="">
+</div>
+</template>
+
+<style>
+.center{
+    text-align: center;
+}
+</style>
